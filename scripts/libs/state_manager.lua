bf5b90234d5cdc249590d39ea6319454a25244e3
SnaveSutit <snavesutit@gmail.com>
2023-10-22 05:20:25 -0400

    Create state_manager.lua

---
+++ scripts/libs/state_manager.lua
@@ -0,0 +1,35 @@
+local lib = {}
+local state = {}
+
+lib.load = function(default)
+	if not fs.exists("state") then
+		state = default or {}
+		return
+	end
+	local file = fs.open("state", "r")
+	state = textutils.unserialize(file.readAll())
+	file.close()
+end
+
+lib.save = function()
+	local file = fs.open("state", "w")
+	file.write(textutils.serialize(state))
+	file.close()
+end
+
+lib.reset = function(default)
+	state = default or {}
+	lib.save()
+end
+
+lib.get = function(key)
+	return state[key]
+end
+
+lib.set = function(key, value)
+	if state[key] == value then return end
+	state[key] = value
+	lib.save()
+end
+
+return lib
