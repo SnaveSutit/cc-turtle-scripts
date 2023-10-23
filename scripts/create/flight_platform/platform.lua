277ce845ef053592272a36f8f1589e53b6c7bf72
SnaveSutit <snavesutit@gmail.com>
2023-10-22 21:59:44 -0400

    Fix incorrect platform diff check

--- scripts/create/flight_platform/platform.lua
+++ scripts/create/flight_platform/platform.lua
@@ -133,7 +133,7 @@ local function main()
 	end
 	saveState()
 
-	if not (distanceX == 0 and distanceZ == 0) then
+	if not (distanceX == 0 or distanceZ == 0) then
 		if distanceZ < 0 then
 			triggerMovement("north")
 		elseif distanceX > 0 then
