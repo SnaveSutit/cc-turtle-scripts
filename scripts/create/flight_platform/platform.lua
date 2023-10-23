0288be383a90695d875710049ceb6d87fba1259d
SnaveSutit <snavesutit@gmail.com>
2023-10-23 00:56:34 -0400

    Update platform to new state system

--- scripts/create/flight_platform/platform.lua
+++ scripts/create/flight_platform/platform.lua
@@ -16,56 +16,50 @@ local net = requireExternal(
 -- "http://localhost:3000/networking.lua"
 	"https://raw.githubusercontent.com/SnaveSutit/cc-turtle-scripts/main/scripts/libs/networking.lua"
 )
+local state = requireExternal(
+-- "http://localhost:3000/state.lua"
+	"https://raw.githubusercontent.com/SnaveSutit/cc-turtle-scripts/main/scripts/libs/state_manager.lua"
+)
 
-local state = {
+local defaultState = {
 	controllerID = nil,
-	position = { x = 0, z = 0 },
-	targetPosition = { x = 0, z = 0 }
+	["targetPosition.x"] = 0,
+	["targetPosition.z"] = 0,
+	["position.x"] = 0,
+	["position.z"] = 0
 }
 
-local function saveState()
-	local file = fs.open(".platform_state", "w")
-	file.write(textutils.serialize(state))
-	file.close()
-end
-
-local function loadState()
-	if not fs.exists(".platform_state") then return end
-	local file = fs.open(".platform_state", "r")
-	state = textutils.unserialize(file.readAll())
-	file.close()
-end
-
 local function lookForControllers()
-	while state.controllerID == nil do
+	local foundID
+	while state.get("controllerID") == nil do
 		print("Looking for controllers...")
 		repeat
-			state.controllerID = rednet.lookup(protocol)
+			foundID = rednet.lookup(protocol)
 			sleep(1)
-		until state.controllerID ~= nil
-		print("Found controller: " .. state.controllerID)
+		until foundID ~= nil
+		print("Found controller: " .. foundID)
 		print("Attempting to link...")
 
-		local success = net.requestFrom(state.controllerID, "snavesutit:link_controller", {
+		local success = net.requestFrom(state.get("controllerID"), "snavesutit:link_controller", {
 			isPlatform = true
 		})
 
 		if not success then
 			print("Failed to link to controller.")
-			state.controllerID = nil
+			state.set("controllerID", nil)
 			sleep(1)
 		else
+			state.set("controllerID", rednet.lookup(protocol))
 			print("Linked!")
-			saveState()
 		end
 	end
 end
 
 local function reconnectController()
 	local tries = 1
-	print("Attempting to reconnect controller: " .. state.controllerID)
+	print("Attempting to reconnect controller: " .. state.get("controllerID") .. "...")
 	local success, connected
-	success = net.requestFrom(state.controllerID, "snavesutit:reconnect", {
+	success = net.requestFrom(state.get("controllerID"), "snavesutit:reconnect", {
 		isPlatform = true
 	}, function(_connected)
 		connected = _connected
@@ -79,8 +73,7 @@ local function reconnectController()
 		return
 	end
 	print("Failed to reconnect to controller. Disconnecting...")
-	state.controllerID = nil
-	saveState()
+	state.set("controllerID", nil)
 end
 
 local directionMap = {
@@ -93,15 +86,14 @@ local directionMap = {
 local function triggerMovement(direction)
 	local side = directionMap[direction]
 	if side == "front" then  -- North
-		state.position.z = state.position.z - 1
+		state.set("position.z", state.get("position.z") - 1)
 	elseif side == "right" then -- East
-		state.position.x = state.position.x + 1
+		state.set("position.x", state.get("position.x") + 1)
 	elseif side == "back" then -- South
-		state.position.z = state.position.z + 1
+		state.set("position.z", state.get("position.z") + 1)
 	elseif side == "left" then -- West
-		state.position.x = state.position.x - 1
+		state.set("position.x", state.get("position.x") - 1)
 	end
-	saveState()
 
 	redstone.setOutput(side, true)
 	sleep(0.05)
@@ -111,27 +103,25 @@ end
 
 local function main()
 	net.init(modem, protocol)
-	loadState()
-	saveState()
+	state.load(".platform_state", defaultState)
 
 	sleep(0.5)
 
-	if state.controllerID ~= nil then
-		state.targetPosition = { x = state.position.x, z = state.position.z }
-		saveState()
+	if state.get("controllerID") ~= nil then
+		state.set("targetPosition.x", state.get("position.x"))
+		state.set("targetPosition.z", state.get("position.z"))
 		reconnectController()
 	end
-	if state.controllerID == nil then
+	if state.get("controllerID") == nil then
 		lookForControllers()
 	end
-	saveState()
 
-	local distanceX = state.targetPosition.x - state.position.x
-	local distanceZ = state.targetPosition.z - state.position.z
+	local distanceX = state.get("targetPosition.x") - state.get("position.x")
+	local distanceZ = state.get("targetPosition.z") - state.get("position.z")
 
 	shell.run("clear")
-	print("Current position: " .. state.position.x .. ", " .. state.position.z)
-	print("Target position: " .. state.targetPosition.x .. ", " .. state.targetPosition.z)
+	print("Current position: " .. state.get("position.x") .. ", " .. state.get("position.z"))
+	print("Target position: " .. state.get("targetPosition.x") .. ", " .. state.get("targetPosition.z"))
 	print("Distance: " .. distanceX .. ", " .. distanceZ)
 
 	if (distanceX ~= 0 or distanceZ ~= 0) then
@@ -150,59 +140,71 @@ local function main()
 	end
 
 	local exit = false
-	while (state.controllerID ~= nil and not exit) do
+	while (state.get("controllerID") ~= nil and not exit) do
 		parallel.waitForAny(
 			function()
-				net.heartbeatSender(state.controllerID)
+				net.heartbeatSender(state.get("controllerID"))
 				print("Lost connection to controller.")
-				state.controllerID = nil
-				saveState()
+				state.set("controllerID", nil)
 				exit = true
 			end,
 			function()
 				-- TODO: Implement controller's side of re-connection
-				net.listenForRequestFrom(state.controllerID, "snavesutit:reconnect", function(data, otherID)
-					if data.isPlatform and state.controllerID == otherID then
+				net.listenForRequestFrom(state.get("controllerID"), "snavesutit:reconnect", function(data, otherID)
+					if data.isPlatform and state.get("controllerID") == otherID then
 						print("Controller Reconnected!")
 					end
 				end)
 			end,
 			function()
 				-- TODO: Implement controller's side of re-connection
-				net.listenForRequestFrom(state.controllerID, "snavesutit:disconnect", function(data, otherID)
-					if data.isController and state.controllerID == otherID then
+				net.listenForRequestFrom(state.get("controllerID"), "snavesutit:disconnect", function(data, otherID)
+					if data.isController and state.get("controllerID") == otherID then
 						print("Controller Disconnected: " .. data.reason)
-						state.controllerID = nil
+						state.set("controllerID", nil)
 						exit = true
 					end
 				end)
 			end,
 			function()
-				net.listenForRequestFrom(state.controllerID, "snavesutit:setzero", function(data)
-					state.position = { x = data.zeroPosition.x, z = data.zeroPosition.z }
-					state.targetPosition = { x = data.zeroPosition.x, z = data.zeroPosition.z }
+				net.listenForRequestFrom(state.get("controllerID"), "snavesutit:setzero", function(data)
+					state.set("position.x", data.zeroPosition.x)
+					state.set("position.z", data.zeroPosition.z)
+					state.set("targetPosition.x", data.zeroPosition.x)
+					state.set("targetPosition.z", data.zeroPosition.z)
 				end)
 				print("Zero set.")
 			end,
 			function()
-				net.listenForRequestFrom(state.controllerID, "snavesutit:settargetpos", function(data)
-					state.targetPosition = { x = data.targetPosition.x, z = data.targetPosition.z }
+				net.listenForRequestFrom(state.get("controllerID"), "snavesutit:settargetpos", function(data)
+					state.set("targetPosition.x", data.targetPosition.x)
+					state.set("targetPosition.z", data.targetPosition.z)
 				end)
-				print("Target position set to " .. state.targetPosition.x .. ", " .. state.targetPosition.z)
+				print("Target position set to " .. state.get("targetPosition.x") .. ", " .. state.get("targetPosition.z"))
 				exit = true
 			end,
 			function()
-				net.listenForRequestFrom(state.controllerID, "snavesutit:setpos", function(data)
-					state.position = { x = data.position.x, z = data.position.z }
+				net.listenForRequestFrom(state.get("controllerID"), "snavesutit:setpos", function(data)
+					state.set("position.x", data.position.x)
+					state.set("position.z", data.position.z)
 				end)
-				print("Target position set to " .. state.targetPosition.x .. ", " .. state.targetPosition.z)
+				print("Position set to " .. state.get("position.x") .. ", " .. state.get("position.z"))
 				exit = true
 			end,
 			function()
 				while true do
-					net.listenForRequestFrom(state.controllerID, "snavesutit:getpos", function()
+					net.listenForRequestFrom(state.get("controllerID"), "snavesutit:getpos", function()
 						print("Position requested.")
-						return { position = state.position, targetPosition = state.targetPosition }
+						return {
+							position = {
+								x = state.get("position.x"),
+								z = state.get("position.z")
+							},
+							targetPosition = {
+								x = state.get("targetPosition.x"),
+								z = state.get("targetPosition.z")
+							}
+						}
 					end)
 					print("Position sent.")
 				end
@@ -211,9 +213,7 @@ local function main()
 		sleep(0.1)
 	end
 
-	saveState()
-	sleep(1)
-	-- os.reboot()
+	os.reboot()
 end
 
 main()
